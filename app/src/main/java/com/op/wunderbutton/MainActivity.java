d7cc7b1ea1ba68ccf01bd737ca5551d4b7859597
picaro <picaro.vs@gmail.com>
2015-04-03 20:22:32 +0200

    added login activity

--- app/src/main/java/com/op/wunderbutton/MainActivity.java
+++ app/src/main/java/com/op/wunderbutton/MainActivity.java
@@ -1,21 +1,22 @@
 package com.op.wunderbutton;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
+import lombok.extern.java.Log;
 
+@Log
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        log.info("init");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
